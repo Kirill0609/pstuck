5a8cd5e76b30bb5aaae24294e2665dc0f8f77af7
wlaub <goldcray@gmail.com>
2016-11-26 15:58:44 -0800

    first commit

---
+++ pstuck.py
@@ -0,0 +1,213 @@
+import win32api
+import win32con
+import win32gui
+import time
+import PIL.ImageGrab as sc
+import itertools
+
+def move((x,y)):
+    win32api.SetCursorPos((x,y))
+
+def down((x,y)):
+    win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN,x,y,0,0)
+
+def up((x,y)):
+    win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP,x,y,0,0)
+
+
+locs = itertools.product(range(5), range(6))
+corner=(1029,301)
+scale=64
+colors =    [ (123, 108, 97) #skull
+            , (128,60,53) #red
+            , (81, 113, 69)  #green
+            , (85,129,152) #blue
+            , (140, 108, 62) #yellow
+            , (99, 60, 136)  #purple
+            ]
+
+colormap = ['s', 'r', 'g', 'b', 'y', 'p']
+
+moves = 0
+
+def getBoardImg():
+    return sc.grab((corner[0], corner[1], corner[0] + 5*scale, corner[1] + 6*scale))
+
+def compareColor(c1, c2):
+    error = 0
+    for l, r in zip(c1, c2):
+        error += (l-r)**2
+    return error
+
+def matchColor(color):
+    errs = [compareColor(color, x) for x in colors]
+    return errs.index(min(errs))
+
+def getOrb(img, pos):
+    r = 0
+    g = 0
+    b = 0
+    for x in range(pos[0]*scale,(pos[0]+1)*scale):
+        for y in range(pos[1]*scale,(pos[1]+1)*scale):
+            p = img.getpixel((x,y))
+            r += p[0]
+            g += p[1]
+            b += p[2]
+    r /= scale*scale
+    b /= scale*scale
+    g /= scale*scale
+    return matchColor((r,g,b))
+
+def getBoard():
+    img = getBoardImg()
+    result = []
+
+    for y in range(0,6):
+        row = [getOrb(img, (x,y)) for x in range(0,5)]
+        result.append(row)
+    return result
+
+def boardToString(board):
+    nboard = []
+    for line in board:
+        row = [colormap[x] for x in line]
+        nboard.append(row)
+    return nboard
+
+def printBoard(board):
+    for row in boardToString(board):
+        print(("{} "*5).format(*row))
+
+def swapBoard(board, start, stop):
+    board[start[1]][start[0]] ^= board[stop[1]][stop[0]]
+    board[stop[1]][stop[0]] ^= board[start[1]][start[0]]
+    board[start[1]][start[0]] ^= board[stop[1]][stop[0]]
+    return board
+
+def coords((x,y)):
+    return int(corner[0] + scale*(x+.5)), int(corner[1] + scale*(y+.5))
+
+def swap(board, start, stop, fake=False):
+    global moves
+    swapBoard(board, start, stop)
+    moves += 1
+    start = coords(start)
+    stop = coords(stop)
+    if not fake:
+        t = [.05, 0, .08]
+        move(start)
+        time.sleep(t[0])
+        down(start)
+        time.sleep(t[1])
+        move(stop)
+        time.sleep(t[2])
+        up(stop)
+
+
+def moveorb(board, start, stop, fake = False):
+    script = []
+    x = start[0]
+    y = start[1]
+    while (x,y) != stop:
+        if x < stop[0]:
+            script.append(((x, y),(x+1,y)))
+            x+=1
+        elif x > stop[0]:
+            script.append(((x, y),(x-1,y)))
+            x-=1
+        elif y < stop[1]:
+            script.append(((x, y),(x,y+1)))
+            y+=1
+        elif y > stop[1]:
+            script.append(((x, y),(x,y-1)))
+            y-=1
+        else:
+            break
+    for a, b in script:
+        swap(board, a,b, fake)
+
+def dist(a,b):
+    return abs(a[0] - b[0])+abs(a[1]-b[1])
+
+def findColors(board, color, exclude = []):
+    clist = []
+    for x in range(5):
+        for y in range(6):
+            if board[y][x] == color and (x,y) not in exclude:
+                clist.append((x,y))
+    return clist
+
+def colorCenter(board):
+    result = []
+    for color in range(len(colors)):
+        clist = findColors(board, color)
+        if len(clist) > 2:
+            x = float(sum([a[0] for a in clist]))/len(clist)
+            y = float(sum([a[1] for a in clist]))/len(clist)
+            result.append((color, x,y))
+    return result
+
+def colorToPoint(board, color, pos, exclude):
+    clist = []
+    if board[pos[1]][pos[0]] == color:
+        return 0,0
+    for x in range(5):
+        for y in range(6):
+            if board[y][x] == color and (x,y) not in exclude and y != pos[1]:
+                clist.append((x,y))
+    if len(clist) == 0:
+        return 0,0
+    errs = [dist(pos, x) for x in clist]
+    best = clist[errs.index(min(errs))]
+    return best, pos
+
+
+def fillRow(board, row, color, lock, fake = False):
+    y = row
+    for x in range(5)[::-1]:
+        start, stop = colorToPoint(board, color, (x, y), lock)
+        lock.append((x,y))
+#        print(start, stop)
+        if start != 0:
+            moveorb(board, start, stop, fake)
+    return lock
+
+#s, r, g, b, y, p
+#0, 1, 2, 3, 4, 5
+
+def doRows(fake = False):
+    global moves
+    moves = 0
+    times = []
+
+    lock = []
+
+    board = getBoard()
+    printBoard(board)
+    weights = colorCenter(board)
+    weights = sorted(weights, key= lambda x: x[2], reverse=True)
+
+    times.append(time.time())
+
+    for i in range(len(weights)):
+        lock = fillRow(board, 5-i, weights[i][0], lock, fake)
+
+    times.append(time.time() - times[-1])
+
+    print("moves: {}".format(moves))
+    if not fake:
+        print("times: {}".format(times[1:]))
+    else:
+        print("times: {}".format(moves*.13))
+    printBoard(board)
+
+    move(coords((6,0)))
+
+
+while 1:
+    time.sleep(.1)
+    if win32api.GetAsyncKeyState(win32con.VK_F1):
+        doRows(True)
+    if win32api.GetAsyncKeyState(win32con.VK_F2):
+        doRows(False)
+
